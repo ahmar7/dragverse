60f2d4a5680bf1cedfd9a1674bc07c9bf2ba5c07
hinay <hinaarooj978@gmail.com>
2022-10-26 00:19:35 +0500

    page update

--- src/components/Mint/index.js
+++ src/components/Mint/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "./mint.css";
+import { Line, MintIcon, StakeIcon, MarketIcon } from "../../utils/allImgs";
 const Mint = () => {
   return (
     <div className="main-container">
-      <div className="mint-main"></div>
+      <div className="mint-main">
+        <div className="icon-bg">
+          <div className="icon-area">
+            <img className="side-icon" src={MintIcon} alt="" />
+            <p>STAKING</p>
+          </div>
+
+          <div className="icon-area">
+            <img className="middle-icon" src={StakeIcon} alt="" />
+            <p>MINT</p>
+          </div>
+          <div className="icon-area">
+            <img className="side-icon" src={MarketIcon} alt="" />
+            <p>Public Marketplace</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/utils/allImgs/index.js
+++ src/utils/allImgs/index.js
@@ -1,3 +1,8 @@
 import MintBg from "../../assets/img/Mint.png";
+import IconBg from "../../assets/img/icon-bg.png";
+import MintIcon from "../../assets/img/mint-icon.png";
+import StakeIcon from "../../assets/img/stake-icon.png";
+import MarketIcon from "../../assets/img/marketplace-icon.png";
+import Line from "../../assets/img/Line 1.png";
 ///////////////////////////////////////////////////// Collection Images
-export { MintBg };
+export { MintBg, IconBg, MintIcon, StakeIcon, MarketIcon, Line };
