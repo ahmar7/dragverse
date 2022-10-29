673079432e85498f893919b7bf9473595085bfb5
hinay <hinaarooj978@gmail.com>
2022-10-29 12:23:49 +0500

    stling update

--- src/components/Stake/index.js
+++ src/components/Stake/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./stake.css";
 import {
   Line,
   MintIcon,
@@ -13,58 +13,16 @@ import {
 const Stake = () => {
   return (
     <div className="main-container">
-      <div className="mint-main">
-        <div className="logo">
+      <div className="main-staking">
+        <div className="side-logo">
           <img src={Logo} alt="" />
         </div>
-        <div className="counter-bg">
-          <div className="mint-counter">
-            <div className="increment">
-              <img className="ico-id" src={Plus} alt="" />
-            </div>
-            <div className="digit">3</div>
-            <div className="decrement">
-              <img className="ico-id" src={Minus} alt="" />
-            </div>
-            <div className="mint-btn">
-              <button>Mint</button>
-            </div>
-          </div>
+        <div className="staking-btn">
+          <button>Staked</button>
+          <button>Not Staked</button>
         </div>
-        <div className="mint-details">
-          <h3>Running Whitelist!</h3>
-          <ul className="mint-address">
-            <li>
-              <span>Sale Status:</span> Presale
-            </li>
-            <li>
-              <span>Address:</span> 0x28294910348934892342
-            </li>
-            <li>
-              <span>Supply:</span> 2/3333
-            </li>
-          </ul>
-        </div>
-        <div className="icon-bg">
-          <div className="icon-area side-icon">
-            <Link to="/">
-              <img src={StakeIcon} alt="" />
-              <p>MINT</p>
-            </Link>
-          </div>
-
-          <div className="icon-area middle-ico">
-            <Link to="/staking">
-              <img className="middle-icon" src={MintIcon} alt="" />
-              <p>STAKING</p>
-            </Link>
-          </div>
-          <div className="icon-area sp side-icon">
-            <Link to="/marketplace">
-              <img src={MarketIcon} alt="" />
-              <p className="mplace">Public Marketplace</p>
-            </Link>
-          </div>
+        <div className="cards">
+          <div className="single-card"></div>
         </div>
       </div>
     </div>
